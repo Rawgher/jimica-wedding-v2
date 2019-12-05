49165e7d0092251966d725c914c9e1552f53b525
Roger <rdpender3@gmail.com>
2019-12-04 20:24:57 -0800

    working on media queries

--- assets/js/ceremony.js
+++ assets/js/ceremony.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
-  var ceremonyTop1 = $("#dayOfItinerary").offset().top;
-  var ceremonyTop2 = $("#bridalParty").offset().top;
-  var ceremonyTop3 = $("#gettingThere").offset().top;
+  if ($(window).width() >= 1025) {
+    var ceremonyTop1 = $("#dayOfItinerary").offset().top;
+    var ceremonyTop2 = $("#bridalParty").offset().top;
+    var ceremonyTop3 = $("#gettingThere").offset().top;
 
-  $(document).scroll(function() {
-    var scrollPos = $(document).scrollTop();
-    if (scrollPos >= ceremonyTop1 && scrollPos < ceremonyTop2) {
-      $("#list-dayOfItinerary").css("color", "#A20519");
-      $("#list-bridalParty").css("color", "#000");
-      $("#list-gettingThere").css("color", "#000");
-    } else if (scrollPos >= ceremonyTop2 && scrollPos < ceremonyTop3) {
-      $("#list-dayOfItinerary").css("color", "#000");
-      $("#list-bridalParty").css("color", "#A20519");
-      $("#list-gettingThere").css("color", "#000");
-    } else if (scrollPos >= ceremonyTop3) {
-      $("#list-dayOfItinerary").css("color", "#000");
-      $("#list-bridalParty").css("color", "#000");
-      $("#list-gettingThere").css("color", "#A20519");
-    }
-  });
+    $(document).scroll(function() {
+      var scrollPos = $(document).scrollTop();
+      if (scrollPos >= ceremonyTop1 && scrollPos < ceremonyTop2) {
+        $("#list-dayOfItinerary").css("color", "#A20519");
+        $("#list-bridalParty").css("color", "#000");
+        $("#list-gettingThere").css("color", "#000");
+      } else if (scrollPos >= ceremonyTop2 && scrollPos < ceremonyTop3) {
+        $("#list-dayOfItinerary").css("color", "#000");
+        $("#list-bridalParty").css("color", "#A20519");
+        $("#list-gettingThere").css("color", "#000");
+      } else if (scrollPos >= ceremonyTop3) {
+        $("#list-dayOfItinerary").css("color", "#000");
+        $("#list-bridalParty").css("color", "#000");
+        $("#list-gettingThere").css("color", "#A20519");
+      }
+    });
+  }
 
   // function used to scroll down to the wedding party section
   $("#bridalButton").click(function() {
--- assets/js/cruise.js
+++ assets/js/cruise.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-  var cruiseTop1 = $("#cruiseItinerary").offset().top;
-  var cruiseTop2 = $("#bookingInfo").offset().top;
-  var cruiseTop3 = $("#whatToBring").offset().top;
-  var cruiseTop4 = $("#activities").offset().top;
+  if ($(window).width() >= 1025) {
+    var cruiseTop1 = $("#cruiseItinerary").offset().top;
+    var cruiseTop2 = $("#bookingInfo").offset().top;
+    var cruiseTop3 = $("#whatToBring").offset().top;
+    var cruiseTop4 = $("#activities").offset().top;
 
-  $(document).scroll(function() {
-    var scrollPos = $(document).scrollTop();
-    if (scrollPos >= cruiseTop1 && scrollPos < cruiseTop2) {
-      $("#list-cruiseItinerary").css("color", "#A20519");
-      $("#list-bookingInfo").css("color", "#000");
-      $("#list-whatToBring").css("color", "#000");
-      $("#list-activities").css("color", "#000");
-    } else if (scrollPos >= cruiseTop2 && scrollPos < cruiseTop3) {
-      $("#list-cruiseItinerary").css("color", "#000");
-      $("#list-bookingInfo").css("color", "#A20519");
-      $("#list-whatToBring").css("color", "#000");
-      $("#list-activities").css("color", "#000");
-    } else if (scrollPos >= cruiseTop3 && scrollPos < cruiseTop4) {
-      $("#list-cruiseItinerary").css("color", "#000");
-      $("#list-bookingInfo").css("color", "#000");
-      $("#list-whatToBring").css("color", "#A20519");
-      $("#list-activities").css("color", "#000");
-    } else if (scrollPos >= cruiseTop4) {
-      $("#list-cruiseItinerary").css("color", "#000");
-      $("#list-bookingInfo").css("color", "#000");
-      $("#list-whatToBring").css("color", "#000");
-      $("#list-activities").css("color", "#A20519");
-    }
-  });
-
+    $(document).scroll(function() {
+      var scrollPos = $(document).scrollTop();
+      if (scrollPos >= cruiseTop1 && scrollPos < cruiseTop2) {
+        $("#list-cruiseItinerary").css("color", "#A20519");
+        $("#list-bookingInfo").css("color", "#000");
+        $("#list-whatToBring").css("color", "#000");
+        $("#list-activities").css("color", "#000");
+      } else if (scrollPos >= cruiseTop2 && scrollPos < cruiseTop3) {
+        $("#list-cruiseItinerary").css("color", "#000");
+        $("#list-bookingInfo").css("color", "#A20519");
+        $("#list-whatToBring").css("color", "#000");
+        $("#list-activities").css("color", "#000");
+      } else if (scrollPos >= cruiseTop3 && scrollPos < cruiseTop4) {
+        $("#list-cruiseItinerary").css("color", "#000");
+        $("#list-bookingInfo").css("color", "#000");
+        $("#list-whatToBring").css("color", "#A20519");
+        $("#list-activities").css("color", "#000");
+      } else if (scrollPos >= cruiseTop4) {
+        $("#list-cruiseItinerary").css("color", "#000");
+        $("#list-bookingInfo").css("color", "#000");
+        $("#list-whatToBring").css("color", "#000");
+        $("#list-activities").css("color", "#A20519");
+      }
+    });
+  }
   // function used to scroll down to the booking info section
   $("#bookingButton").click(function() {
     $("html, body").animate(
